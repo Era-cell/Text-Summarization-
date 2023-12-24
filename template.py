6070bcc9f17cb829da06c4b952757eb6b82b71e5
Suprith KG <suprith7kg@gmail.com>
2023-12-24 10:44:37 +0530

    Template created

---
+++ template.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+folder = "text-summarization"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "app.py",
+    "params.yaml",
+    "setup.py",
+    "main.py",
+    "config/config.yaml",
+    "Dockerfile",
+    "requirements.txt",
+    "research/trial.ipynb"
+    f"src/{folder}/__init__.py"
+    f"src/{folder}/config/configuration.py",
+    f"src/{folder}/entity/__init__.py",
+    f"src/{folder}/config/__init__.py",
+    f"src/{folder}/utils/__init__.py",
+    f"src/{folder}/utils/common.py",
+    f"src/{folder}/components/__init__.py",
+    f"src/{folder}/constants/__init__.py",
+    f"src/{folder}/logging.py",
+    f"src/{folder}/pipeline/__init__.py",
+    "template.py"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
